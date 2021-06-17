f26c80062d04085bc6c1fc9fea1227e5be3987a1
IBrazyI <73790113+IBrazyI@users.noreply.github.com>
2021-06-17 17:25:15 +0100

    installed pymong and connected app to mongo database

--- env.py
+++ env.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP","0.0.0.0")
 os.environ.setdefault("PORT","5000")
 os.environ.setdefault("SECRET_KEY", "ThAZ3qZ8zW")
-os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://braz_josh:zaxxiffron@cluster0.ikcaw.mongodb.net/the_drunken_goblin?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DB", "the_drunken_goblin")
